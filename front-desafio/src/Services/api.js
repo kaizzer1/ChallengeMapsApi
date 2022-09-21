a1f746d7e0044dd95ec98ede993c10bcdb67a6b7
Lucas Kaizer <lucaskaizer1@hotmail.com>
2022-09-21 02:48:46 -0300

    front

--- front-desafio/src/Services/api.js
+++ front-desafio/src/Services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "desafiounicad-production.up.railway.app",
+  baseURL: "https://desafiounicad-production.up.railway.app",
 });
 
 export default api;
